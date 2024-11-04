cb7966fc7026323e1b6a8177e304a9529f2366f5
xuyaokun_kzx <304813473@qq.com>
2024-11-04 14:10:50 +0800

    impr:demo例子提交

--- src/main/java/cn/com/kun/component/demo/springtest/SpringTester.java
+++ src/main/java/cn/com/kun/component/demo/springtest/SpringTester.java
@@ -1,14 +1,10 @@
 package cn.com.kun.component.demo.springtest;
 
-import org.elasticsearch.action.get.GetRequest;
-
 public class SpringTester {
 
     public static void main(String[] args) {
 
         System.out.println("SpringTester");
-
-        GetRequest getRequest = new GetRequest();
     }
 
 }
